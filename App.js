d8d61bd436b48034d55b7de0eec4a781b44f8ea1
ChabaJosa <chabagjg@gmail.com>
2022-04-21 22:20:06 -0400

    Created base rick and morty api interface

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // https://rickandmortyapi.com/api/character/?page=1
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    getData();
+  }, []);
+  //
+  async function getData() {
+    const req = await fetch(
+      "https://rickandmortyapi.com/api/character/?page=1"
+    );
+    const res = await req.json();
+    // console.log(res)
+    await setData(res.results);
+  }
+  //
+  console.log("Heree ", data);
+  //
+  if (data !== null) {
+    return (
+      <View style={styles.container}> 
+        {data.map((item, index) => {
+          return <Text key={index}>{item.name}</Text>;
+        })}
+      </View>
+    );
+  } else {
+    return <Text>Loading...</Text>;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
